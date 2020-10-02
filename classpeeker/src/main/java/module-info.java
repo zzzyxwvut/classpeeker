88722106b7b82c3be0d793914cba2d85067fe3e9
Aliaksei Budavei <oytis@yandex.ru>
2020-10-02 17:42:14 +0300

    Create a classpeeker module declaration

---
+++ classpeeker/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+/** Defines a means of class inspection. */
+@SuppressWarnings({ "requires-automatic", "requires-transitive-automatic" })
+module org.zzzyxwvut.classpeeker
+{
+	requires transitive commons.cli;
+}
